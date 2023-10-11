0e493452fea37b1e382a066b44ea7c31e5839b16
Cozma Rares <cozmarares11@gmail.com>
2023-10-11 11:07:04 +0300

    refactor: remove header component

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,21 @@ import SideBar from "@/components/SideBar";
 import { DndContext } from "@dnd-kit/core";
 import DragOverlayWrapper from "@/components/DragOverlayWrapper";
 import EditorBlocksContextProvider from "@/context/editor-blocks";
-import Header from "@/components/Header";
+import ImportButton from "./components/ImportButton";
+import ExportButton from "./components/ExportButton";
+import ThemeSwitch from "./components/ThemeSwitch";
 
 const App = () => {
   return (
     <>
       <EditorBlocksContextProvider>
         <div className="grid h-screen grid-cols-[auto,minmax(0,1fr)] grid-rows-[auto,minmax(0,1fr)]">
-          <Header />
+          <nav className="col-span-full flex items-center justify-end gap-3 px-6 py-2 shadow shadow-black">
+            <h1 className="mr-auto text-2xl font-semibold">Code Canvas</h1>
+            <ImportButton />
+            <ExportButton />
+            <ThemeSwitch />
+          </nav>
           <DndContext>
             <SideBar />
             <Editor />

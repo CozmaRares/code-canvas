72d7bce8761d2bc376ab8cea7de9587f944a4596
Cozma Rares <cozmarares11@gmail.com>
2023-10-16 17:27:13 +0300

    refactor: sidebar accordion

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -5,30 +5,45 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import SideBarButton from "@/components/SideBarButton";
+import { CodeBlockType } from "./blocks/utils/code-block";
+
+type AccordionProps = Record<string, CodeBlockType[]>;
+
+const accordionProps: AccordionProps = {
+  "variables & values": ["variable assign", "variable name", "number"],
+};
 
 const SideBar = () => (
   <aside className="w-[300px] border-r border-black p-8 dark:border-white">
-    <Accordion
-      type="single"
-      className="w-full [&_ul]:space-y-2 [&_ul]:p-1"
-    >
-      <AccordionItem value="variables & values">
-        <AccordionTrigger className="font-normal">
-          Variables & Values
+    <AccordionBuilder {...accordionProps} />
+  </aside>
+);
+
+const AccordionBuilder = (props: AccordionProps) => (
+  <Accordion
+    type="single"
+    className="w-full [&_ul]:space-y-2 [&_ul]:p-1"
+  >
+    {Object.entries(props).map(([key, blockTypes]) => (
+      <AccordionItem
+        value={key}
+        key={key}
+      >
+        <AccordionTrigger className="font-normal capitalize">
+          {key}
         </AccordionTrigger>
         <AccordionContent>
           <ul>
-            <li>
-              <SideBarButton type="variable assign" />
-            </li>
-            <li>
-              <SideBarButton type="variable name" />
-            </li>
+            {blockTypes.map(type => (
+              <li key={type}>
+                <SideBarButton type={type} />
+              </li>
+            ))}
           </ul>
         </AccordionContent>
       </AccordionItem>
-    </Accordion>
-  </aside>
+    ))}
+  </Accordion>
 );
 
 export default SideBar;
